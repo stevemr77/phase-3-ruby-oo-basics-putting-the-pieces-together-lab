e79535fac72861ca23ca187ab96b7163867d3eaa
Michael Ramirez <stevemr77@gmail.com>
2021-06-16 15:18:20 -0600

    commit tree c243ab55ec560d55150c49fff80a4aee304610c4

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,36 @@
+class Book
+
+    #attr_accessor :page_count
+    attr_reader :title, :author, :genre, :page_count
+
+    def initialize(title= "And Then There Were None")
+        @title = title
+    end
+
+    def author
+        @author = author
+    end
+
+    def page_count
+        @page_count = page_count
+    end
+
+    def genre
+        @genre = genre
+    end
+
+    #def turn_page
+     #   @page_count += 1
+    #end
+
+    
+    
+    
+    
+end
 
 
+Book.new 
+Book.author = "Agatha Christie"
+Book.page_count = 272
+Book.genre = "Mystery"
